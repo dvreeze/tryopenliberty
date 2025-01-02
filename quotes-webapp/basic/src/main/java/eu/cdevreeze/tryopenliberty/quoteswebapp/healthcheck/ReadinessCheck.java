db760ddd06bef8806f655bb5588376c83f7c6553
Chris de Vreeze <chris.de.vreeze@caiway.net>
2025-01-02 23:56:17 +0100

    Added health checks

---
+++ quotes-webapp/basic/src/main/java/eu/cdevreeze/tryopenliberty/quoteswebapp/healthcheck/ReadinessCheck.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2024-2024 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.tryopenliberty.quoteswebapp.healthcheck;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+/**
+ * Readiness health check. Use path "health/ready" to access it.
+ *
+ * @author Chris de Vreeze
+ */
+@Readiness
+@ApplicationScoped
+public class ReadinessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        var msg = ReadinessCheck.class.getSimpleName() + " Readiness Check";
+        return HealthCheckResponse.up(msg);
+    }
+}
